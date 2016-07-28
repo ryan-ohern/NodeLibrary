0524816295bd2106efa827e8b3388369e8ca1b82
Ryan O'Hern <ryanohern@gmail.com>
2016-07-28 11:28:40 -0400

    modules and require

---
+++ 1.6_modules and require.js
@@ -0,0 +1,16 @@
+// takes an array as a parameter
+var counter = function(arr){
+	return 'There are ' + arr.length + ' elements in this array';
+};
+
+// call counter and pass in an array
+console.log(counter(['shaun', 'crystal', 'ryu']));
+
+// to make function available outside of this module
+module.exports = counter;
+
+/////////////////////////////////// INSIDE app.js ///////////////////////////////////////////
+// to access count.js
+var counter = require('./count');
+
+console.log(counter(['shaun', 'crystal', 'ryu']));
---
+++ app.js
@@ -0,0 +1,4 @@
+// to access count.js
+var counter = require('./count');
+
+console.log(counter(['shaun', 'crystal', 'ryu']));
